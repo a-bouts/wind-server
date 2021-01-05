b011a7a85c1477e457c333844698f35b1d2ec8aa
a-bouts <arnaud.bouts@gmail.com>
2021-01-05 09:35:48 +0100

    add cache header to wind data

--- api.go
+++ api.go
@@ -27,6 +27,10 @@ func InitServer(n *Noaa) *mux.Router {
 	router.HandleFunc("/winds/-/healthz", s.healthz).Methods(http.MethodGet)
 	router.HandleFunc("/winds", s.getWindsHandler).Methods(http.MethodGet)
 	router.HandleFunc("/winds/{forecast}", s.getWindHandler).Methods(http.MethodGet)
+	router.HandleFunc("/winds/{forecast}/{stamp}", s.getWindHandlerByStampOld).Methods(http.MethodGet)
+
+	apiV1 := router.PathPrefix("/winds/api/v1").Subrouter()
+	apiV1.HandleFunc("/", s.getWindsHandler).Methods("POST")
 	router.HandleFunc("/winds/{forecast}/{stamp}", s.getWindHandlerByStamp).Methods(http.MethodGet)
 
 	return router
@@ -82,7 +86,7 @@ func (s server) getWindsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s server) getWindHandlerByStamp(w http.ResponseWriter, r *http.Request) {
+func (s server) getWindHandlerByStampOld(w http.ResponseWriter, r *http.Request) {
 
 	forecast := mux.Vars(r)["forecast"]
 	stamp := mux.Vars(r)["stamp"]
@@ -116,6 +120,44 @@ func (s server) getWindHandlerByStamp(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+}
+
+func (s server) getWindHandlerByStamp(w http.ResponseWriter, r *http.Request) {
+
+	forecast := mux.Vars(r)["forecast"]
+	stamp := mux.Vars(r)["stamp"]
+
+	log.Println(forecast, stamp)
+
+	if _, found := s.n.Forecasts[forecast]; !found {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	for _, f := range s.n.Forecasts[forecast] {
+		if stamp == strings.Split(f, ".")[0] {
+			in, err := os.Open("json-data/" + f)
+			if err != nil {
+				fmt.Println("Error", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			defer in.Close()
+
+			_, err = io.Copy(w, in)
+			if err != nil {
+				fmt.Println("Error", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Cache-Control", "public, max-age=10800, immutable")
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func (s server) getWindHandler(w http.ResponseWriter, r *http.Request) {
